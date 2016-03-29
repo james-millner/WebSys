23286ed5a19dbb26cb0f79c80c52ab7917a88ff1
james-millner <u1257434@unimail.hud.ac.uk>
2016-03-29 15:25:05 +0100

    Added Stub model with getters and setters to represent comments.

---
+++ src/main/java/com/jm/WebSys/domain/Comment.java
@@ -0,0 +1,68 @@
+package com.jm.WebSys.domain;
+
+import java.util.Date;
+
+/**
+ * Created by James on 29/03/2016.
+ */
+public class Comment {
+
+    String id;
+    String rid;
+    Date timestamp;
+    String author;
+    String comment;
+
+    //No Arg Constructor
+    public Comment() {
+
+    }
+
+    public Comment(String i, String r, Date t, String a, String c){
+        this.id = i;
+        this.rid = r;
+        this.timestamp = t;
+        this.author = a;
+        this.comment = c;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
