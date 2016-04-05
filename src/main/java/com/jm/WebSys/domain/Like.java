2f5b01a9c44e521f9958571c3b102aa0d6f4c6a8
james-millner <u1257434@unimail.hud.ac.uk>
2016-04-05 20:02:46 +0100

    Adding stub for allowing the user to like different recipes.

---
+++ src/main/java/com/jm/WebSys/domain/Like.java
@@ -0,0 +1,46 @@
+package com.jm.WebSys.domain;
+
+/**
+ * Created by James on 05/04/2016.
+ */
+public class Like {
+
+    String id;
+    String rid;
+    String uid;
+
+    //No Arg Constructor
+    public Like() {
+
+    }
+
+    public Like(String i, String r, String u) {
+        this.id = i;
+        this.rid = r;
+        this.uid = u;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
